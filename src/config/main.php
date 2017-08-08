328e463f7d9299ff6d381a4e8afff1dd533fb646
Denis Utkin <dizirator@gmail.com>
2017-08-08 19:37:12 +0100

    Added configs

---
+++ src/config/main.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @author Denis Utkin <dizirator@gmail.com>
+ * @link   https://github.com/dizirator
+ */
+
+return [
+    'modules' => [
+        'content' => 'setrun\content\Module'
+    ]
+];
